a33db86bb5922dabbead7d123b2884bc695ae2f9
Anjaneyulu <anjaneyulu@kreatio.com>
2015-06-08 16:29:48 +0530

    new stple

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,9 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = false
-  config.assets.compress = false
+  #config.assets.compress = false
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
